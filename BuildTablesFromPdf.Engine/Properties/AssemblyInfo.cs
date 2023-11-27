edc7ad2c6626e8e5855de1ee7579a5d75e69eb2a
bubibubi <u.ballestrazzi@gmail.com>
2023-11-27 16:58:33 +0100

    -Version update

--- BuildTablesFromPdf.Engine/Properties/AssemblyInfo.cs
+++ BuildTablesFromPdf.Engine/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BuildTablesFromPdf.Engine")]
-[assembly: AssemblyCopyright("Copyright © 2017-2022")]
+[assembly: AssemblyCopyright("Copyright © 2017-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
